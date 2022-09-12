1c2451a604342c8824f5683f9c1ccce975367d26
Adel Sen <adelsenhadjii@gmail.com>
2022-09-12 15:36:20 +0200

    Fix: fix jokes types

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,7 +8,7 @@ export class AppService {
 
   async getJoke(): Promise<Joke> {
     const response = await this.httpService.get(
-      'https://v2.jokeapi.dev/joke/Programming,Spooky?lang=fr&blacklistFlags=nsfw,religious,political,racist,sexist,explicit',
+      'https://v2.jokeapi.dev/joke/Programming,Spooky?lang=fr
       {
         headers: {
           accept: 'application/json',
